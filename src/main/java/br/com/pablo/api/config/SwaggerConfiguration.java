dcb2fbcf06a4295676d5b243dd5966244c15cd75
pablojg9 <pablojuniorgn2@gmail.com>
2022-02-06 22:29:36 -0300

    add resourceHandler in swagger ui

--- src/main/java/br/com/pablo/api/config/SwaggerConfiguration.java
+++ src/main/java/br/com/pablo/api/config/SwaggerConfiguration.java
@@ -1,5 +1,6 @@
-package br.com.pablo.api.config.swagger;
+package br.com.pablo.api.config;
 
+import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.client.LinkDiscoverer;
@@ -7,8 +8,12 @@ import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,8 +23,7 @@ import java.util.List;
 
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
-public class SwaggerConfiguration {
+public class SwaggerConfiguration extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket api(){
@@ -35,5 +39,15 @@ public class SwaggerConfiguration {
         List<LinkDiscoverer> discoverers = new ArrayList<>();
         discoverers.add(new CollectionJsonLinkDiscoverer());
         return new LinkDiscoverers(SimplePluginRegistry.create(discoverers));
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
